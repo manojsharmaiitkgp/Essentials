c2dbe585fb9da108b92a0f66f61d7bd09765175e
Manoj Kumar Sharma <manojsharma.net@gmail.com>
2017-11-11 20:54:23 +0530

    Add files via upload

---
+++ CallingSeparateFile.lua
@@ -0,0 +1,6 @@
+-- This file demonstrate the use of multiple file in torch
+
+dofile("supportFile.lua")
+obj = supportFile()
+x=obj:add(a,2)
+print('input=',x)
---
+++ supportFile.lua
@@ -0,0 +1,5 @@
+local supportFile = torch.class("supportFile")
+function supportFile:paddnow(input,value)
+out=input+value
+return out
+end
